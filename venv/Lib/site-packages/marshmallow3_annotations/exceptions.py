c3ad5242ba9bff527f6e2d095fac8dbd3055c368
Your Nameruhuma <your.email@example.comruhumaj@gmail.com>
2024-05-14 13:19:57 +0530

    Update .gitignore to exclude __pycache__ and venv directories

--- venv/Lib/site-packages/marshmallow3_annotations/exceptions.py
+++
@@ -1,9 +0,0 @@
-from marshmallow.exceptions import MarshmallowError
-
-
-class MarshmallowAnnotationError(MarshmallowError):
-    pass
-
-
-class AnnotationConversionError(MarshmallowAnnotationError):
-    pass
